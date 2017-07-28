942eed1e2d4109da518369d99a6bbcfd43fdc883
Jess Gardner <inanna1130@gmail.com>
2017-07-28 16:36:32 -0400

    Rendered homepage index route

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('homepage index test')
-  res.send('HOMEPAGE')
-  // res.render('index', { title: 'Express' });
+  // res.send('HOMEPAGE')
+  res.render('index', { title: 'Crowded!' });
 });
 
 module.exports = router;
